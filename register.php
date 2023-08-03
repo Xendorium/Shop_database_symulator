e416fd71e5081799e2a8cb20a3870172c69941d3
Xendorium <akama23@wp.pl>
2023-08-03 22:26:45 +0200

    create register comand

--- register.php
+++ register.php
@@ -7,3 +7,7 @@ Password: <br/> <input type="password" name="password"> <br/>
 Email: <br/> <input type="text" name="email"> <br/>
     <input type="submit" value="Register">
 </form>
+<?php
+    echo $_SESSION['error'];
+    $_SESSION['error']="";
+?>
--- registerdb.php
+++ registerdb.php
@@ -6,26 +6,27 @@
     }
     else{
         mysqli_select_db($polaczenie, $database);
+
         $name=$_POST['name'];
         $surname=$_POST['surname'];
         $login=$_POST['login'];
         $passwordc=$_POST['password'];
         $email=$_POST['email'];
+
         $sql="SELECT idklienta FROM klienci WHERE login='$login'";
         $rezults=mysqli_query($polaczenie, $sql);
-        if($rezults!=""){
-           echo "This login is already used";
-           header('Location:register.php');
-           exit();
-        }
-        $sql="SELECT idklienta FROM klienci WHERE email='$email'";
-        $rezults=mysqli_query($polaczenie, $sql);
-        if($rezults!=""){
-            echo "This email is already used";
+        if(isset($rezults)){
+            $_SESSION['error']="This login is already used";
             header('Location:register.php');
             exit();
         }
-
+        $sql="SELECT idklienta FROM klienci WHERE email='$email'";
+        $rezults=mysqli_query($polaczenie, $sql);
+        if($rezults!=NULL){
+            $_SESSION['error']="This email is already used";
+            header('Location:register.php');
+            exit();
+        }
         /*$sql="INSERT INTO klienci VALUES(NULL,'$name','$surname','$login','$passwordc','$email')";
         mysqli_query($polaczenie, $sql);*/
         header('Location:index.php');
