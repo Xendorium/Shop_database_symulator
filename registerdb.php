45e14eca8802a9136a3fa081c376e6844a4d53dc
Xendorium <akama23@wp.pl>
2023-08-03 21:46:50 +0200

    create register comand

--- registerdb.php
+++ registerdb.php
@@ -1,6 +1,5 @@
 <?php
     require_once 'dbconnect.php';
-    require_once 'register.php';
     $polaczenie = mysqli_connect($host,$user,$password);
     if($polaczenie->connect_errno!=0) {
         echo "Error" .$polaczenie->connect_errno;
@@ -12,6 +11,23 @@
         $login=$_POST['login'];
         $passwordc=$_POST['password'];
         $email=$_POST['email'];
+        $sql="SELECT idklienta FROM klienci WHERE login='$login'";
+        $rezults=mysqli_query($polaczenie, $sql);
+        if($rezults!=""){
+           echo "This login is already used";
+           header('Location:register.php');
+           exit();
+        }
+        $sql="SELECT idklienta FROM klienci WHERE email='$email'";
+        $rezults=mysqli_query($polaczenie, $sql);
+        if($rezults!=""){
+            echo "This email is already used";
+            header('Location:register.php');
+            exit();
+        }
+
+        /*$sql="INSERT INTO klienci VALUES(NULL,'$name','$surname','$login','$passwordc','$email')";
+        mysqli_query($polaczenie, $sql);*/
         header('Location:index.php');
         exit();
     }
